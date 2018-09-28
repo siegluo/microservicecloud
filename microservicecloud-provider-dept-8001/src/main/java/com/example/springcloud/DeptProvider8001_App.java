a7b8b5a1078519d1cf2aba5721e3105ffa9ae3ce
roger <1357852347@qq.com>
2018-09-28 10:45:01 +0800

    annotation

--- microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/DeptProvider8001_App.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
-@EnableDiscoveryClient //服务发现
+//本服务启动后会自动注册进eureka服务中
+@EnableEurekaClient
+//服务发现
+@EnableDiscoveryClient
 public class DeptProvider8001_App
 {
 	public static void main(String[] args)
